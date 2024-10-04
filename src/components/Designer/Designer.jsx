461f03d164d3a879220e9f83c47b08135d795b58
Bruno-Delourme <delourmebruno@gmail.com>
2024-10-04 08:43:07 +0200

    rename page Coder by Designer

--- src/components/Designer/Designer.jsx
+++ src/components/Designer/Designer.jsx
@@ -8,10 +8,10 @@ const Designer = () => {
             <section className="Designer-section">
                 <div className="Designer-content">
                     <h1>about.</h1>
-                    <p>I&aposm a product designer based in Paris, France. Well, I mean I live in aubervilliers. It&aposs technically suburb. But there is a Metro close to my appartement. So I&aposm in Paris.</p>
-                    <p>Since 2015, I&aposve enjoyed turning complex problems into simple, beautiful and intuitive designs. 
+                    <p><h2>I&aposm a product designer based in Paris, France. Well, I mean I live in aubervilliers. It&aposs technically suburb. But there is a Metro close to my appartement. So I&aposm in Paris.</h2></p>
+                    <p><h3>Since 2015, I&aposve enjoyed turning complex problems into simple, beautiful and intuitive designs. 
                         When I&aposm not pushing pixels, you&aposll find me playing League of Legends, with my dog or trying to figure it out how to download the last movie I saw on Cinema, but I was to lazy to move cause I was playing League of Legends... and I&aposm not even good at it. Well... I&aposPlat III...
-                    </p>
+                    </h3></p>
                 </div>
                 <div className="Designer-gallery">
                     <img src="/path-to-image1.jpg" alt="gallery-img-1" />
