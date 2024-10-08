5f522269cb70cce34fc00ec50de91b256fc40457
Bruno-Delourme <delourmebruno@gmail.com>
2024-10-08 19:35:23 +0200

    Add About page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/Home/Home';
 import Portfolio from './components/Portfolio/Portfolio';
 import Contact from './components/Contact/Contact';
 import Header from './components/Header/Header';
+import About from './components/About/About';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
         
       </Routes>
     </Router>
---
+++ src/components/About/About.jsx
@@ -0,0 +1,40 @@
+import './About.css';
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <div className="about-content">
+        {/* Part Designer */}
+        <div className="part part-designer">
+          <h2>Part designer</h2>
+          <ul>
+            <li>UI design</li>
+            <li>UX design</li>
+            <li>Design Systems</li>
+            <li>Interaction design</li>
+            <li>&quot;Making it pop&quot;</li>
+          </ul>
+        </div>
+
+        {/* Placeholder for Pie Chart */}
+        <div className="pie-placeholder">
+          {/* The pie chart will be added here later */}
+        </div>
+
+        {/* Part Coder */}
+        <div className="part part-coder">
+          <h2>Part coder</h2>
+          <ul>
+            <li>Front-end development</li>
+            <li>HTML / CSS</li>
+            <li>JavaScript (kinda)</li>
+            <li>Swearing at my computer</li>
+            <li>Eating pizza</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
