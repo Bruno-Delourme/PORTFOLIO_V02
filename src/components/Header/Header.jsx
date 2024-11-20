ebbc8f77785bd908f8ccd59c694cb3d7f54e7c97
Bruno-Delourme <delourmebruno@gmail.com>
2024-11-20 18:13:06 +0100

    add logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
+import BD_logo_white from '../../assets/BD_logo_white.png';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -15,25 +16,27 @@ const Header = () => {
 
   return (
     <header className="header">
-      <div className="logo">
-        <Link to="/" onClick={closeMenu}>LOGO</Link>
-      </div>
-      
-      <div 
-        className={`burger-menu ${isMenuOpen ? 'active' : ''}`}
-        onClick={toggleMenu}
-      >
-        <div className="burger-line"></div>
-        <div className="burger-line"></div>
-        <div className="burger-line"></div>
-      </div>
+      <div className="header-content">
+        <Link to="/" className="logo" onClick={closeMenu}>
+          <img src={BD_logo_white} alt="Bruno Delourme Logo" className="header-logo" />
+        </Link>
+        
+        <div 
+          className={`burger-menu ${isMenuOpen ? 'active' : ''}`}
+          onClick={toggleMenu}
+        >
+          <div className="burger-line"></div>
+          <div className="burger-line"></div>
+          <div className="burger-line"></div>
+        </div>
 
-      <nav className={`nav ${isMenuOpen ? 'active' : ''}`}>
-        <Link to="/" onClick={closeMenu}>Home</Link>
-        <Link to="/portfolio" onClick={closeMenu}>Portfolio</Link>
-        <Link to="/about" onClick={closeMenu}>About</Link>
-        <Link to="/contact" onClick={closeMenu}>Contact</Link>
-      </nav>
+        <nav className={`nav ${isMenuOpen ? 'active' : ''}`}>
+          <Link to="/" onClick={closeMenu}>Home</Link>
+          <Link to="/portfolio" onClick={closeMenu}>Portfolio</Link>
+          <Link to="/about" onClick={closeMenu}>About</Link>
+          <Link to="/contact" onClick={closeMenu}>Contact</Link>
+        </nav>
+      </div>
     </header>
   );
 };
